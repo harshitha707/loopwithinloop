dbef12d2d7965c4bd5e6e01f1539c2f7f1afff85
harshitha707 <53364952+harshitha707@users.noreply.github.com>
2024-02-01 17:55:00 +0530

    Update main.rs

--- loopwithinloop/src/main.rs
+++ loopwithinloop/src/main.rs
@@ -1,13 +1,13 @@
 fn main() {
     let days = ["first", "second", "third", "fourth", "fifth"];
-    let animals = ["A partridge in a pear tree", "Two turtle doves", "Three french hens", "Four calling birds", "Five gold rings"];
+    let lyrics = ["A partridge in a pear tree", "Two turtle doves", "Three french hens", "Four calling birds", "Five gold rings"];
     //for loop accessing index in days and length
     for i in 0..days.len() {
         //printing the first sentence 
         println!("On the {} day of Christmas my true love sent to me",days[i]);
         for j in (0..=i).rev() {
             //accessing the j for equal to i
-            println!("{} ", animals[j])
+            println!("{} ", lyrics[j]);
         }
         println!("");
     }
